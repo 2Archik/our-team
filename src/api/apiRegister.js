aeec89a5b39a928c6b56d5d425eb8c36ce2737e9
Artur Ladin <artur.avitolog@yandex.ru>
2024-06-22 21:20:34 +0500

    ended register page

--- src/api/apiRegister.js
+++ src/api/apiRegister.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "https://reqres.in/api/";
 
-export const register = async (data) => {
+export const registration = async (data) => {
   try {
     const response = await axios.post(`${BASE_URL}register`, data);
     return response;
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,64 +1,98 @@
 import { useForm } from "react-hook-form";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { register } from "../../api/apiRegister";
+import { registration } from "../../api/apiRegister";
 import styles from "./styles.module.css";
+import { useState } from "react";
 
 function Register() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    watch,
+  } = useForm();
+
   const navigate = useNavigate();
 
-  const [data, setData] = useState({ email: "", password: "" });
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    const response = await register(data);
+  const onSubmit = async (data) => {
+    const response = await registration(data);
     const token = response.data.token;
     localStorage.setItem("token", token);
     navigate("/main");
   };
 
-  const onInputChange = (name, value) => {
-    setData((prev) => ({ ...prev, [name]: value }));
+  const [visiblePassword, setVisiblePassword] = useState(true);
+
+  const onVisiblePasswords = () => {
+    setVisiblePassword(!visiblePassword);
   };
 
   return (
     <div className={styles.layout}>
       <div className={styles.content}>
-        <form className={styles.form} onSubmit={onSubmit}>
+        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
           <h2>Регистрация</h2>
           <label className={styles.label}>
             Имя
-            <input type="text" name="name" placeholder="Eve" required />
+            <input
+              className={errors?.name ? styles.redOutline : ""}
+              placeholder="Eve"
+              {...register("name", {
+                required: "true",
+              })}
+            />
+            <div>{errors?.name && <p>Ошибка</p>}</div>
           </label>
           <label className={styles.label}>
             Электронная почта
             <input
-              type="email"
-              name="email"
-              value={data.email}
+              className={errors?.email ? styles.redOutline : ""}
+              type="text"
               placeholder="eve.holt@reqres.in"
-              required
-              onChange={(e) => onInputChange("email", e.target.value)}
+              {...register("email", {
+                required: "true",
+                pattern: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-z]{2,4}$/,
+              })}
             />
+            <div>{errors?.email && <p>Ошибка</p>}</div>
           </label>
           <label className={styles.label}>
             Пароль
             <input
-              type="password"
-              name="password"
-              value={data.password}
+              className={errors?.password ? styles.redOutline : ""}
+              type={visiblePassword ? "password" : "text"}
               placeholder="pistol"
-              required
-              onChange={(e) => onInputChange("password", e.target.value)}
+              {...register("password", {
+                required: "true",
+              })}
+            />
+            <div>{errors?.password && <p>Ошибка</p>}</div>
+            <img
+              className={styles.icon}
+              src="../../.././public/eye.svg"
+              onClick={onVisiblePasswords}
             />
           </label>
           <label className={styles.label}>
             Подтвердите пароль
             <input
-              type="password"
-              name="confirmPassword"
+              className={errors?.confirmPassword ? styles.redOutline : ""}
+              type={visiblePassword ? "password" : "text"}
               placeholder="pistol"
-              required
+              {...register("confirmPassword", {
+                required: "true",
+                validate: (val) => {
+                  if (watch("password") != val) {
+                    return "Ошибка";
+                  }
+                },
+              })}
+            />
+            <div>{errors?.confirmPassword && <p>Ошибка</p>}</div>
+            <img
+              className={styles.icon}
+              src="../../.././public/eye.svg"
+              onClick={onVisiblePasswords}
             />
           </label>
           <button type="submit" className={styles.btn}>
